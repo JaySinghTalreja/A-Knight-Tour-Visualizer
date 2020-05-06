a1798aaaf669b68ade705e67d1d63134e30cf973
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-05-06 12:16:19 +0530

    Added Moves, Ranks And Flies

---
+++ warnsdorff.js
@@ -0,0 +1,18 @@
+class WarnsdorffAlgorithm{
+    constructor(rows = 8, cols = 8) {
+        this.row = row;
+        this.cols = cols;
+        this.moves = [
+            [1,2],
+            [1,-2],
+            [2, 1],
+            [2, -1],
+            [-1, 2],
+            [-1, -2],
+            [-2, 1],
+            [-2, -2],
+        ];
+        this.files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        this.ranks = [8, 7, 6, 5, 4, 3, 2, 1];
+    }
+}
