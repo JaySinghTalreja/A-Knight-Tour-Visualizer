33c8fea3c956f7667372f46dc68cc610e6fc0f54
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-05-06 12:22:18 +0530

    Chess Board Canvas

--- warnsdorff.js
+++ warnsdorff.js
@@ -2,6 +2,14 @@ class WarnsdorffAlgorithm{
     constructor(rows = 8, cols = 8) {
         this.rows = rows;
         this.cols = cols;
+        //Chess Board Prep using Canvas
+
+        
+        this.chessBoard = document.createElement('canvas');
+        this.chessBoardContext = this.chessBoard.getContext('2d');
+
+
+        //Set of Knight Moves
         this.moves = [
             [1,2],
             [1,-2],
@@ -12,9 +20,12 @@ class WarnsdorffAlgorithm{
             [-2, 1],
             [-2, -2],
         ];
+
         this.files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
         this.ranks = [8, 7, 6, 5, 4, 3, 2, 1];
         this.totalNumberOfTiles = this.cols * this.rows;
-        
+    }
+    initChessBoard() {
+
     }
 }
