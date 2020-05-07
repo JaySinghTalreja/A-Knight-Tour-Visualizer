3f0bcc30f2f385c80beebefdd6a3f7c5d6c62feb
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-05-07 17:19:29 +0530

    drawChessBoardLabel Done

--- warnsdorff.js
+++ warnsdorff.js
@@ -93,17 +93,61 @@ class WarnsdorffAlgorithm {
 
         this.drawChessBoardBase(0, 0, chessBoardBaseX * sqTileSize, chessBoardBaseY * sqTileSize);
 
-        for(var i =0;i<=this.cols;i++) {
+        for(var i =0;i<=cols + 1;i++) {
             const offsetCols = i * sqTileSize;
             if(i > 1 && i < chessBoardBaseX) {
-                
+                this.drawChessBoardLabel('File', sqTileSize / 2, offsetCols, i);
             }
         }
+
+        for(let j=0;j<=rows + 1;j+=1) {
+            const offsetRows = j * sqTileSize;
+            if(j > 1 && j < chessBoardBaseY) {
+                this.drawChessBoardLabel('rank', sqTileSize / 2 , offsetRows, j);
+            }
+        }
+        chessTiles.map(row=>row.map( col => {
+            const offsetX = sqTileSize + (col.position);
+        }));
     }
 
     drawChessBoardBase(x, y, width, height) {
         const { chessBoardContext } = this;
         chessBoardContext.fillStyle = '#FF5733';
         chessBoardContext.fillRect(x, y, width, height);
+    }
+
+    drawText(value, x, y, font = '16px sans-serif') {
+        const { chessBoardContext } = this;
+        chessBoardContext.font = font;
+        chessBoardContext.textAlign = 'center';
+        chessBoardContext.textBaseline = 'middle';
+        chessBoardContext.fillStyle = '#000';
+        chessBoardContext.fillText(value, x, y);
+      }
+
+    drawChessBoardLabel(type, center, offset, count) {
+        const { sqTileSize, cols, rows } = this;
+        const size = sqTileSize;
+        const width = cols * size;
+        const height = rows * size;
+        const files = this.files.slice(0, cols);
+        const ranks = this.ranks.slice(this.ranks.length - rows);
+
+        const double = size * 2;
+        const right = double + (width - center);
+        const bottom = double + (height - center);
+        const align = offset - center;
+
+        const font = '16px sans-serif';
+        const file = files[count - 2];
+        if(type === 'File') {
+            this.drawText(file, align, center, font);
+            this.drawText(file, align, bottom, font);
+        } 
+        else if(type === 'rank') {
+            this.drawText(rank, center, align, font);
+            this.drawText(rank, right, align , font);
+        }
     }
 }
