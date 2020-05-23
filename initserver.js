5173e85eb5a3986135963bea14e7754af4f23455
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-05-23 23:02:37 +0530

    Final Commit

--- initserver.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const morgan = require('morgan');
-
-app.use(morgan('dev'))
-    .use('/', express.static(path.join(__dirname, '/')))
-    .listen(3000, console.log('Server Started on Port 3000'));
